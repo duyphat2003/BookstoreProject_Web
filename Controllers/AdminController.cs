e2df75f614dbc07c1f7db43fe98b1e32be7e1986
hoangninh2003 <128906612+hoangninh2003@users.noreply.github.com>
2023-09-20 22:37:16 +0700

    ninh

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,26 @@
-﻿namespace BookstoreProject.Controllers
+﻿using BookstoreProject.Firestore_Database;
+using BookstoreProject.Models;
+using Firebase.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace BookstoreProject.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+            //_auth = new FirebaseAuthProvider(new FirebaseConfig(_apiKey));
+        }
+
+        public IActionResult Index()
+        {
+            BookstoreProjectDatabase.ConnectToFirestoreDB();
+            return View();
+        }
     }
 }
