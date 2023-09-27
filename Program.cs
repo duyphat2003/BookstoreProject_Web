65cf17297c0615a0c01ab81cbbfd7728c5d38ac2
duyphat2003 <89205718+duyphat2003@users.noreply.github.com>
2023-09-27 08:12:11 +0700

    Rút gọn dòng code trong bookstoreprojectdatabase

--- Program.cs
+++ Program.cs
@@ -41,5 +41,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+  //  pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
